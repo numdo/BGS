d1f4f1a9e9150ed454e819a0d0f5551a6e494447
이명성 <goodthinglee@gmail.com>
2025-01-21 13:40:39 +0900

    Initial commit

---
+++ backend/bgs/src/main/java/com/ssafy/bgs/BgsApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.bgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BgsApplication.class, args);
+	}
+
+}
---
+++ backend/bgs/src/test/java/com/ssafy/bgs/BgsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.bgs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BgsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
