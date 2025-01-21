503e54149a31af3f81e57a670e75de19dba3368d
LeeGukgeon <lkukun0223@gmail.com>
2025-01-21 17:50:35 +0900

    project create

---
+++ frontend/bgs/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <>
+      <h1>불끈성 사이트</h1>
+    </>
+  )
+}
+
+export default App
