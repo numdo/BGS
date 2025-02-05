75576cec8d5057794aa14abd64103e187c1ae205
LeeGukgeon <lkukun0223@gmail.com>
2025-02-05 17:53:41 +0900

    create api folder

---
+++ frontend/bgs/src/api/Evaluation.jsx
@@ -0,0 +1,27 @@
+import axios from "axios";
+const BASE_URL = "https://i12c209.p.ssafy.io/";
+
+
+export async function getEvaluations() {
+
+}
+
+export async function getEvaluation() {
+
+}
+
+export async function createEvaluation() {
+
+}
+
+export async function updateEvaluation() {
+
+}
+
+export async function deleteEvaluation() {
+
+}
+
+export async function vote() {
+
+}
--- frontend/bgs/src/api/Image.jsx
+++ frontend/bgs/src/api/Image.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 const BASE_URL = "https://i12c209.p.ssafy.io/";
 
-export async function follow() {
+export async function patchImage() {
 
 }
