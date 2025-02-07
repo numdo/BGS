6b371cf5ed2fcc4bdcf7ad205f8d1ff7b93a5ca7
김도현 <kdhehsl@gmail.com>
2025-02-08 02:32:25 +0900

    hotfix : axiosInstance url 서버로 변경

--- frontend/bgs/src/utils/axiosInstance.jsx
+++ frontend/bgs/src/utils/axiosInstance.jsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://i12c209.p.ssafy.io/api",
   headers: {
     "Content-Type": "application/json",
   },
