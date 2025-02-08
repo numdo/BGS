1c7122415404c45d39b089210e6b19e017b1b54e
이명성 <goodthinglee@gmail.com>
2025-02-08 22:13:19 +0900

    refactor: 필요없는 필드 삭제

--- backend/bgs/src/main/java/com/ssafy/bgs/ai/config/STTConfig.java
+++ backend/bgs/src/main/java/com/ssafy/bgs/ai/config/STTConfig.java
@@ -1,29 +1,17 @@
 package com.ssafy.bgs.ai.config;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
+@Getter
+@Setter
 @Configuration
 public class STTConfig {
-
-    @Value("${stt.api.key}")
-    private String apiKey;
-
     @Value("${stt.api.url}")
     private String apiUrl;
 
     @Value("${stt.language}")
     private String language;
-
-    public String getApiKey() {
-        return apiKey;
-    }
-
-    public String getApiUrl() {
-        return apiUrl;
-    }
-
-    public String getLanguage() {
-        return language;
-    }
 }
