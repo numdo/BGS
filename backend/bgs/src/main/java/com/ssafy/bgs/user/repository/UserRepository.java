ac065edcda95853931bdb002ad29e2f8f091b141
김도현 <kdhehsl@gmail.com>
2025-02-11 16:45:52 +0900

    feat : 관리자 페이지 api 개발 | 관리자 role 추가

--- backend/bgs/src/main/java/com/ssafy/bgs/user/repository/UserRepository.java
+++ backend/bgs/src/main/java/com/ssafy/bgs/user/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package com.ssafy.bgs.user.repository;
 
 import com.ssafy.bgs.user.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -13,4 +15,6 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findBySocialId(String socialId);
 
     User findByNickname(String nickname);
+
+    Page<User> findByNicknameContainingIgnoreCase(String nickname, Pageable pageable);
 }
