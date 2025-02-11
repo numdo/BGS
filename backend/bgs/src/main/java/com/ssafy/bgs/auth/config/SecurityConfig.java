85f1463f5f4b57baf07cec442be2ad8b494e3a13
김도현 <kdhehsl@gmail.com>
2025-02-12 01:20:15 +0900

    fix : role 에 따른 filter 적용

--- backend/bgs/src/main/java/com/ssafy/bgs/auth/config/SecurityConfig.java
+++ backend/bgs/src/main/java/com/ssafy/bgs/auth/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,6 +19,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import java.util.List;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @RequiredArgsConstructor
 public class SecurityConfig {
 
