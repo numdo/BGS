f510eb769434d8210862b4aaebf1d94bd4e473b0
SSAFY <wdse123@naver.com>
2025-02-12 10:31:42 +0900

    refactor: ItemController 세분화
    - 관리자 권한은 AdminItemContoller로 분리
    - 관리자 권한 접근 시 권한 확인 추가

---
+++ backend/bgs/src/main/java/com/ssafy/bgs/mygym/controller/AdminItemController.java
@@ -0,0 +1,67 @@
+package com.ssafy.bgs.mygym.controller;
+
+import com.ssafy.bgs.mygym.dto.response.ItemResponseDto;
+import com.ssafy.bgs.mygym.entity.Item;
+import com.ssafy.bgs.mygym.service.AdminItemService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@PreAuthorize("hasRole('ADMIN')")
+@RequestMapping("/api/admin/items")
+public class AdminItemController {
+
+    private final AdminItemService adminItemService;
+
+    public AdminItemController(AdminItemService adminItemService) {
+        this.adminItemService = adminItemService;
+    }
+
+
+    @GetMapping
+    public ResponseEntity<?> getItemList(
+            @AuthenticationPrincipal Integer userId
+    ) {
+        List<ItemResponseDto> items = adminItemService.getItemList(userId);
+        return new ResponseEntity<>(items, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> addItem(
+            @RequestPart(name = "item") Item item,
+            @RequestPart(name = "file") MultipartFile file
+    ) {
+        adminItemService.addItem(item, file);
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PatchMapping("/{itemId}")
+    public ResponseEntity<?> updateItem(
+            @PathVariable Integer itemId,
+            @RequestPart(name = "item") Item item,
+            @RequestPart(required = false, name = "file") MultipartFile file
+    ) {
+        item.setItemId(itemId);
+        adminItemService.updateItem(item, file);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PatchMapping("/{itemId}/enable")
+    public ResponseEntity<?> enableItem(@PathVariable Integer itemId) {
+        adminItemService.enableItem(itemId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PatchMapping("/{itemId}/disable")
+    public ResponseEntity<?> disableItem(@PathVariable Integer itemId) {
+        adminItemService.disableItem(itemId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
--- backend/bgs/src/main/java/com/ssafy/bgs/mygym/controller/ItemController.java
+++ backend/bgs/src/main/java/com/ssafy/bgs/mygym/controller/ItemController.java
@@ -1,13 +1,11 @@
 package com.ssafy.bgs.mygym.controller;
 
 import com.ssafy.bgs.mygym.dto.response.ItemResponseDto;
-import com.ssafy.bgs.mygym.entity.Item;
 import com.ssafy.bgs.mygym.service.ItemService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -27,39 +25,6 @@ public class ItemController {
     ) {
         List<ItemResponseDto> items = itemService.getItemList(userId);
         return new ResponseEntity<>(items, HttpStatus.OK);
-    }
-
-    @PostMapping
-    public ResponseEntity<?> addItem(
-            @RequestPart(name = "item") Item item,
-            @RequestPart(name = "file") MultipartFile file
-    ) {
-        itemService.addItem(item, file);
-
-        return new ResponseEntity<>(HttpStatus.CREATED);
-    }
-
-    @PatchMapping("/{itemId}")
-    public ResponseEntity<?> updateItem(
-            @PathVariable Integer itemId,
-            @RequestPart(name = "item") Item item,
-            @RequestPart(required = false, name = "file") MultipartFile file
-    ) {
-        item.setItemId(itemId);
-        itemService.updateItem(item, file);
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    @PatchMapping("/{itemId}/enable")
-    public ResponseEntity<?> enableItem(@PathVariable Integer itemId) {
-        itemService.enableItem(itemId);
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    @PatchMapping("/{itemId}/disable")
-    public ResponseEntity<?> disableItem(@PathVariable Integer itemId) {
-        itemService.disableItem(itemId);
-        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PostMapping("/{itemId}/buy")
